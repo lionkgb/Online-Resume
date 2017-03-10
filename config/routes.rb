dc1afb18a5561e9038057564d218c794f471df42
plantejosue <plantejosh1@gmail.com>
2017-03-10 13:06:54 +0200

    added angular, routes, and index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/pages' => 'pages#index'
+  get '/pages/new' => 'pages#new'
+  post '/pages' => 'pages#create'
+  get '/pages/:id' => 'pages#show'
+  get '/pages/:id/edit' => 'pages#edit'
+  patch '/pages/:id' => 'pages#update'
+  delete '/pages/:id' => 'pages#destroy'
 end
